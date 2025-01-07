ca755e4e16241877488bbdf71e06f27a5127567c
Inna Pristupa <inna.g.pristupa@yandex.ru>
2025-01-07 13:38:29 +0300

    красота в код

--- message.py
+++ message.py
@@ -2,6 +2,7 @@
 import pygame
 from vars import *
 
+
 class Message:
     def __init__(self, text):
         self.image = pygame.image.load(CURRENT_DIRECTORY + '/images/message.png')
@@ -10,6 +11,8 @@ class Message:
     def render(self, surface):
         self.surface.blit(self.image, (0, 0))
         scale = pygame.transform.scale(
-            self.surface, (self.surface.get_width() // 3 * 2, self.surface.get_height() // 3 * 2))
+            self.surface,
+            (self.surface.get_width() // 3 * 2, self.surface.get_height() // 3 * 2)
+        )
         scale_rect = scale.get_rect(center=(WIDTH_GAME // 2, HEIGHT_GAME // 2))
         surface.blit(scale, scale_rect)
